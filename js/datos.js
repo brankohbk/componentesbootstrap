4508be30319f4559b47871a7c9f4e915885882d6
brankohbk <brankoh@gmail.com>
2020-08-11 21:10:48 -0300

    Commit inicial

---
+++ js/datos.js
@@ -0,0 +1,183 @@
+const grupos = {
+  "notfound": {
+    nombre: "404 Not found.",
+    rutaFotos: "../img/404/",
+    logo: "logo.jpg"
+  },
+  "cq": {
+    nombre: "Code Queens.",
+    rutaFotos: "../img/cq/",
+    logo: "logo.jpg"
+  },
+  "ctrlz": {
+    nombre: "CTRL Z.",
+    rutaFotos: "../img/ctrlz/",
+    logo: "logo.png"
+  },
+  "pd": {
+    nombre: "Programame el desayuno.",
+    rutaFotos: "../img/pd/",
+    logo: "logo.png"
+  },
+  "semicolon": {
+    nombre: "It was the semicolon.",
+    rutaFotos: "../img/semicolon/",
+    logo: "logo.jpg"
+  }
+}
+const alumnas = [
+  {
+    nombre: "Aylén Díaz García",
+    grupo: "notfound",
+    foto: "aylen.jpg"
+  },
+  {
+    nombre: "Carolina Meneces",
+    grupo: "notfound",
+    foto: "carolina.jpg"
+  },
+  {
+    nombre: "Florencia Velázquez",
+    grupo: "notfound",
+    foto: "florencia.jpg"
+  },
+  {
+    nombre: "Ma. Gabriela Vallenilla",
+    grupo: "notfound",
+    foto: "gabriela.jpg"
+  },
+  {
+    nombre: "Verónica Apaza",
+    grupo: "notfound",
+    foto: "veronica.jpg"
+  },
+  {
+    nombre: "Yael Correa",
+    grupo: "notfound",
+    foto: "yael.jpg"
+  },
+  {
+    nombre: "Andrea briceño",
+    grupo: "cq",
+    foto: "andrea.jpg"
+  },
+  {
+    nombre: "Belén Funes",
+    grupo: "cq",
+    foto: "belen.jpg"
+  },
+  {
+    nombre: "Florencia Mordcovici",
+    grupo: "cq",
+    foto: "florencia.jpg"
+  },
+  {
+    nombre: "Janneth Chura Yujra",
+    grupo: "cq",
+    foto: "janneth.jpg"
+  },
+  {
+    nombre: "Luana Fernandez Brandan",
+    grupo: "cq",
+    foto: "luana.jpg"
+  },
+  {
+    nombre: "Nadine Kamiya",
+    grupo: "cq",
+    foto: "nadine.jpg"
+  },
+  {
+    nombre: "Catalina Moragas",
+    grupo: "ctrlz",
+    foto: "catalina.jpg"
+  },
+  {
+    nombre: "Belén López",
+    grupo: "ctrlz",
+    foto: "belen.jpg"
+  },
+  {
+    nombre: "Johelda Llanos",
+    grupo: "ctrlz",
+    foto: "johelda.jpg"
+  },
+  {
+    nombre: "Luz Cuello",
+    grupo: "ctrlz",
+    foto: "luz.jpg"
+  },
+  {
+    nombre: "Magalí Grabow",
+    grupo: "ctrlz",
+    foto: "magali.jpg"
+  },
+  {
+    nombre: "Yennifer García",
+    grupo: "ctrlz",
+    foto: "yennifer.jpg"
+  },
+  {
+    nombre: "Florencia Rodriguez Teixido",
+    grupo: "pd",
+    foto: "florencia.jpg"
+  },
+  {
+    nombre: "Katherine Maldonado",
+    grupo: "pd",
+    foto: "katherine.jpg"
+  },
+  {
+    nombre: "Malena Colomba",
+    grupo: "pd",
+    foto: "malena.jpg"
+  },
+  {
+    nombre: "Melissa Juárez",
+    grupo: "pd",
+    foto: "melissa.jpg"
+  },
+  {
+    nombre: "Nicole Copa",
+    grupo: "pd",
+    foto: "nicole.jpg"
+  },
+  {
+    nombre: "Victoria Visoná",
+    grupo: "pd",
+    foto: "victoria.jpg"
+  },
+  {
+    nombre: "Azucena Panez Campos",
+    grupo: "semicolon",
+    foto: "azucena.jpg"
+  },
+  {
+    nombre: "Camila Ocamica",
+    grupo: "semicolon",
+    foto: "camila.jpg"
+  },
+  {
+    nombre: "Macarena Munilla",
+    grupo: "semicolon",
+    foto: "macarena.jpg"
+  },
+  {
+    nombre: "Micaela Mignone",
+    grupo: "semicolon",
+    foto: "micaela.jpg"
+  },
+  {
+    nombre: "Reyna Cucho Colan",
+    grupo: "semicolon",
+    foto: "reyna.png"
+  },
+  {
+    nombre: "Rocío Pidemunt",
+    grupo: "semicolon",
+    foto: "rocio.jpg"
+  }
+]
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,89 @@
+
+const container = document.getElementById("container");
+
+window.addEventListener("load", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const equipo = urlParams.get('equipo');
+  equipo && equipo != "todos" ? alumnasPorEquipo(equipo) : (equipo ? acordeonGrupos() : null);
+})
+
+function alumnasPorEquipo(equipo) {
+  container.innerHTML = "";
+  alumnas
+    .filter(alumna => alumna.grupo == equipo)
+    .forEach(alumna => {
+      const card = document.createElement("div");
+      card.className = "card col-12 col-md-4 col-lg-3 m-3 px-0 border-success shadow-lg";
+
+      const img = document.createElement("img");
+      img.src = grupos[alumna.grupo].rutaFotos + alumna.foto;
+      img.alt = alumna.nombre;
+      img.className = "card-img-top";
+
+      const nombre = document.createElement("h5");
+      nombre.className = "card-title m-2";
+      nombre.innerText = alumna.nombre;
+
+      const cardBody = document.createElement('div');
+      cardBody.className = "card-body";
+      cardBody.innerHTML = `<p class="card-text">${grupos[alumna.grupo].nombre}</p>`;
+
+      card.appendChild(img);
+      card.appendChild(nombre);
+      card.appendChild(cardBody);
+
+      container.appendChild(card)
+
+    })
+}
+
+function dibujarGrupos() {
+  container.innerHTML = "";
+  for (grupo in grupos) {
+    const img = document.createElement("img");
+    img.src = grupos[grupo].rutaFotos + grupos[grupo].logo;
+    img.alt = grupos[grupo].nombre;
+    img.className = "col-12 col-sm-6 col-md-4 col-lg-2 my-3";
+    container.appendChild(img)
+  }
+}
+
+function acordeonGrupos() {
+  container.innerHTML = "";
+  const acordeon = document.createElement("div");
+  acordeon.className = "accordion col-9";
+  acordeon.id = "accordionEquipos";
+  for (grupo in grupos) {
+    const acordeonItem = document.createElement("div");
+    acordeonItem.className ="card"
+    acordeonItem.innerHTML = `<div class="card-header" id="heading${grupo}">
+    <h2 class="mb-0">
+      <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse"
+        data-target="#collapse${grupo}" aria-expanded="true" aria-controls="collapse${grupo}">
+        ${grupos[grupo].nombre} 
+      </button>
+    </h2>
+  </div>
+
+  <div id="collapse${grupo}" class="collapse" aria-labelledby="heading${grupo}" data-parent="#accordionEquipos">
+    <div class="card-body">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img src="${grupos[grupo].rutaFotos + grupos[grupo].logo}" class="card-img" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title"> ${grupos[grupo].nombre} </h5>
+            <p class="card-text">Uno de los equipos de grossas que cursan el Bootcamp "Start" para convertirse
+              en <b>desarrolladoras FrontEnd</b>.</p>
+            <a class="btn btn-primary" href="alumnas.html?equipo=${grupo}">Integrantes</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`
+    acordeon.appendChild(acordeonItem)
+
+  }
+  container.appendChild(acordeon)
+}
